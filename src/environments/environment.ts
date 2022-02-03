bc42726e1adf6957976a8b9c38155a6ca6dbf21a
unknown <dirk.mueller2011@hotmail.de>
2022-02-03 08:18:48 +0100

    firebase endversion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'ring-of-fire-cf656',
-    appId: '1:605927030581:web:5943278e61a284912f167c',
-    storageBucket: 'ring-of-fire-cf656.appspot.com',
-    apiKey: 'AIzaSyDLr9r6l86xOFELXOo4N_CTI0GcvIbzjwY',
-    authDomain: 'ring-of-fire-cf656.firebaseapp.com',
-    messagingSenderId: '605927030581',
-  },
-  production: false
+  // firebase: {
+  //   projectId: 'ring-of-fire-cf656',
+  //   appId: '1:605927030581:web:5943278e61a284912f167c',
+  //   storageBucket: 'ring-of-fire-cf656.appspot.com',
+  //   apiKey: 'AIzaSyDLr9r6l86xOFELXOo4N_CTI0GcvIbzjwY',
+  //   authDomain: 'ring-of-fire-cf656.firebaseapp.com',
+  //   messagingSenderId: '605927030581',
+  // },
+  
 };
 
 /*
